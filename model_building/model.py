edff66b134da4c84b4b71dff39063ba1dfd9807e
Arthur <arthurmlcaldas@outlook.com>
2021-06-10 05:06:52 -0300

    defined features using rfe

--- model_building/model.py
+++ model_building/model.py
@@ -13,44 +13,38 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.feature_selection import RFE
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 
-
-
+#directories
 BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
 DATA_DIR = os.path.join(BASE_DIR, 'feature_eng', 'data', 'ft_df.csv')
 
 df = pd.read_csv(DATA_DIR)
 
-df.info()
-
+#dropping columns one wouldn't have before an actual match
 cols_to_drop = ['season', 'match_name','date', 'home_team', 'away_team', 'home_score', 'away_score',
                 'h_match_points', 'a_match_points']
 
 df.drop( columns = cols_to_drop, inplace = True)
 
-df['winner'] = np.where(df.winner == 'HOME_TEAM', 2, np.where(df.winner == 'AWAY_TEAM', 1, 0))
-
+#filling NAs
 df.fillna(-33, inplace = True)
 
+#turning the target variable into integers
+df['winner'] = np.where(df.winner == 'HOME_TEAM', 2, np.where(df.winner == 'AWAY_TEAM', 1, 0))
+
+#turning categorical into dummy vars
 df_dum = pd.get_dummies(df)
 
-selected_features = ['h_odd', 'd_odd', 'a_odd', 'ht_rank', 
-        'ht_days_ls_match', 'ht_l_points',
-        'ht_l_goals', 'ht_goals_sf',
-        'ht_l_wavg_goals_sf', 
-       'ht_win_streak', 'ht_loss_streak',
-       'at_rank', 'at_days_ls_match','at_l_points',
-       'at_l_goals', 'at_goals_sf',
-        'at_l_wavg_goals_sf', 
-       'at_win_streak', 'at_loss_streak', 'at_l_wavg_goals', 'ht_l_wavg_goals']
+np.random.seed(101)
        
-
 X = df_dum.drop(columns = ['winner'], axis = 1)
-X = X[selected_features]
 y = df_dum.winner.values
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 101)
+#splitting into train and test set to check which model is the best one to work on
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 101)
 
+#scaling features
 scaler = MinMaxScaler()
 
 X_train = scaler.fit_transform(X_train)
@@ -67,35 +61,9 @@ for model, name in zip(models, names):
     scores = cross_val_score(model, X_train, y_train ,scoring= 'accuracy', cv=5)
     print(name, ":", "%0.3f, +- %0.3f" % (scores.mean(), scores.std()), " - Elapsed time: ", time.time() - start)
 
-np.random.seed(101)
 
-X = df_dum.drop(columns = ['winner'], axis = 1)
-y = df_dum.winner.values
+#Creating loop to test which set of features is the best one for Logistic Regression
 
-clf = LogisticRegression(max_iter = 1000, multi_class = 'multinomial')
-
-rfe = RFE(estimator = clf, n_features_to_select = 13, step=1)
-rfe.fit(X, y)
-X, y = rfe.transform(X, y)
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
-
-scaler = MinMaxScaler()
-
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
-
-
-scores = cross_val_score(clf, X_train, y_train ,scoring= 'accuracy', cv=5)
-print(" Clf result :", "%0.3f, +- %0.3f" % (scores.mean(), scores.std()))
-
-a = pd.DataFrame(selector.support_,
-                                   index = X.columns,
-                                    columns=['importance']).sort_values('importance', ascending = True)
-
-a[a.importance == True].index.tolist()
-
-#RFE --------------------------------------------------------------------------
 acc_results = []
 n_features = []
 
@@ -103,9 +71,13 @@ for i in range(5, 40):
     X = df_dum.drop(columns = ['winner'], axis = 1)
     y = df_dum.winner.values
 
-    selector = RFE(LogisticRegression(max_iter= 1000, multi_class = 'multinomial'), n_features_to_select = i, step=1)
-    selector.fit_transform(X, y)
+    np.random.seed(101)
 
+    clf = LogisticRegression(max_iter = 1000, multi_class = 'multinomial')
+
+    rfe = RFE(estimator = clf, n_features_to_select = i, step=1)
+    rfe.fit(X, y)
+    X = rfe.transform(X)
 
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
 
@@ -113,9 +85,6 @@ for i in range(5, 40):
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.fit_transform(X_test)
-
-    clf = LogisticRegression(max_iter = 1000, multi_class = 'multinomial')
-
 
     start = time.time()
     scores = cross_val_score(clf, X_train, y_train ,scoring= 'accuracy', cv=5)
@@ -126,26 +95,75 @@ for i in range(5, 40):
 plt.plot(n_features, acc_results)
 plt.show()
 
+#getting the best 13 features from RFE
+X = df_dum.drop(columns = ['winner'], axis = 1)
+y = df_dum.winner.values
 
-for i in range(X.shape[1]):
-	print('Column: %d, Selected %s, Rank: %.3f' % (i, selector.support_[i], selector.ranking_[i]))
-
-a = pd.DataFrame(selector.support_,
-                                   index = X.columns,
-                                    columns=['importance']).sort_values('importance', ascending = True)
-
-a[a.importance == True].index.tolist()
-
-
-
-#end of rfe ---------------------------------------
-
+np.random.seed(101)
 
 clf = LogisticRegression(max_iter = 1000, multi_class = 'multinomial')
-clf.fit(X_train, y_train)
 
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
+rfe = RFE(estimator = clf, n_features_to_select = 13, step=1)
+rfe.fit(X, y)
+
+X[rfe.get_support()]
+
+X_transformed = rfe.transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_transformed,y, test_size = 0.2)
+
+scaler = MinMaxScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+scores = cross_val_score(clf, X_train, y_train ,scoring= 'accuracy', cv=5)
+print(" Clf result :", "%0.3f, +- %0.3f" % (scores.mean(), scores.std()))
+
+featured_columns = pd.DataFrame(rfe.support_,
+                            index = X.columns,
+                            columns=['is_in'])
+
+featured_columns = featured_columns[featured_columns.is_in == True].index.tolist()
+featured_columns                            
+
+importances = pd.DataFrame(rfe.ranking_,
+                            index = X.columns,
+                            columns=['ranking']).sort_values('ranking', ascending = True)[importances.ranking == 1]
+
+
+#tuning logistic regression
+
+lr = LogisticRegression(max_iter = 1000, multi_class = 'multinomial')
+
+parameters = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+ 'fit_intercept': (True, False), 'solver' : ('newton-cg', 'sag', 'saga', 'lbfgs'), 'class_weight' : (None, 'balanced')}
+
+gs = GridSearchCV(lr,parameters,scoring='accuracy',cv=3)
+start = time.time()
+
+#printing best fits and time elapsed
+gs.fit(X_train,y_train)
+print(gs.best_score_, gs.best_params_,  time.time() - start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+gb = GradientBoostingClassifier()
+gb.fit(X_train, y_train)
 
 result = clf.predict(X_test)
 
@@ -223,4 +241,21 @@ df['pls'] = df.teste == df.winner
 
 df.pls.mean()
 
-df.to_excel('eohq.xlsx', index = False)'''
+df.to_excel('eohq.xlsx', index = False)
+
+
+
+
+selected_features = ['h_odd', 'd_odd', 'a_odd', 'ht_rank', 
+        'ht_days_ls_match', 'ht_l_points',
+        'ht_l_goals', 'ht_goals_sf',
+        'ht_l_wavg_goals_sf', 
+       'ht_win_streak', 'ht_loss_streak',
+       'at_rank', 'at_days_ls_match','at_l_points',
+       'at_l_goals', 'at_goals_sf',
+        'at_l_wavg_goals_sf', 
+       'at_win_streak', 'at_loss_streak', 'at_l_wavg_goals', 'ht_l_wavg_goals']
+
+
+
+'''
